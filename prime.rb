78d1cc5a5e7760b7b837b9374936badda0ac2c18
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-10 13:16:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,3 @@
 def prime?(y)
-  new_array = []
-  array = (2..y-1).to_a
-  array.each do |z|
-    new_array << y % z 
-  end 
-  !new_array.include?(0) && y>1
+  !(2..y-1).to_a.collect{|z|y%z}.include?(0) && y>1
 end 
